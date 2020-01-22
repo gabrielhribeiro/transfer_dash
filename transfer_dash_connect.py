51eecd63e0ef267480260679690bdc184104e244
Gabriel Ribeiro <gabrielrdavid@gmail.com>
2020-01-22 18:01:26 -0300

    second version

--- transfer_dash_connect.py
+++ transfer_dash_connect.py
@@ -1,5 +1,7 @@
 import yaml
 import mysql.connector
+import pymssql
+
 
 #tool to help connection between databases.
 
@@ -25,19 +27,15 @@ class ConnectionsDB:
         return print(self.conf_get()['mysql']['host'])
 
     def Mysql_Connection(self):
-        cnx = mysql.connector.connect(host=self.conf_get()['mysql']['host'], user=self.conf_get()['mysql']['user'],
-                                      password=self.conf_get()['mysql']['passwd'], database=self.conf_get()['mysql']['db'],
-                                      port=self.conf_get()['mysql']['port'])
-
-        return print(cnx.is_connected())
+        mysql.connector.connect(host=self.conf_get()['mysql']['host'], user=self.conf_get()['mysql']['user'],
+                                password=self.conf_get()['mysql']['passwd'], database=self.conf_get()['mysql']['db'],
+                                port=self.conf_get()['mysql']['port'])
 
     def Sql_Connection(self):
-        cnx = mysql.connector.connect(host=self.conf_get()['ssql']['host'], user=self.conf_get()['ssql']['user'],
-                                      password=self.conf_get()['ssql']['passwd'], database=self.conf_get()['ssql']['db'],
-                                      port=self.conf_get()['ssql']['port'])
-
-        return print(cnx.is_connected())
+        pymssql.connect(host=self.conf_get()['ssql']['host'], user=self.conf_get()['ssql']['user'],
+                        password=self.conf_get()['ssql']['passwd'], database=self.conf_get()['ssql']['db'],
+                        port=self.conf_get()['ssql']['port'], as_dict=self.conf_get()['ssql']['as_dict'],
+                        login_timeout=self.conf_get()['ssql']['login_timeout'])
 
 if __name__ == '__main__':
     ConnectionsDB()
-    teste
