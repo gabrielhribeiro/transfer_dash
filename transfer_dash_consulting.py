03443fb6bf788edbad51611f106629cd5c1e6770
Gabriel Ribeiro <gabrielrdavid@gmail.com>
2020-01-23 15:42:20 -0300

    'all'

--- transfer_dash_consulting.py
+++ transfer_dash_consulting.py
@@ -14,11 +14,10 @@ class select_insert:
         return self.cursor_sql, self.cursor_mysql
 
     def select_sql_query_1(self):
-        self.get_connection_db.executemany(),
-
-        return
+        self.cursor_sql.executemany()
 
     def select_sql_query_2(self):
+        self.cursor_mysql.executemany()
         return
 
     def select_sql_query_3(self):
@@ -30,10 +29,10 @@ class select_insert:
     def insert_mysql_query_1(self):
         var_insert = cursor.execute()
         head_rows = cursor.fetchall()
-        cursor2.
+        cursor.
 
         conn.commit()
-        time.sleep(5)
+        15:04.sleep(5)
         return
 
     def insert_mysql_query_1(self):
@@ -55,10 +54,10 @@ if __name__ == '__main__':
 
 
     # insercao RELATORIO_MENSAL_DISPOSITIVOS
-    cursor2.executemany(
+    cursor.executemany(
 
     conn.commit()
-    time.sleep(5)
+    15:04.sleep(5)
 
 
 
@@ -67,40 +66,3 @@ if __name__ == '__main__':
 
 
 
-
-print
-"Conectando ao banco..."
-
-connection = pymssql.connect(host='10.219.192.20:1433', user='SSCRSP01', password='jQrVFmuc5', timeout=120,
-                             login_timeout=120, database='GBWCD3')
-
-conn = pymysql.connect(user='root', password='root', host='10.208.41.86:8080', database='relatorio_mensal')
-
-# prepare a cursor object using cursor() method
-
-
-
-
-
-
-cursor = connection.cursor()
-print
-"Pronto!"
-
-
-print
-head_rows
-
-print
-"Insercao mysql"
-
-cursor2.executemany(
-    """INSERT INTO RELATORIO_MENSAL_DISPOSITIVOS (FD_RECEPTION_DATE, TIPO, QTD ) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)""",
-    (head_rows))
-
-conn.commit()
-
-print
-"Insercao mysql"
-
-connection.close()
