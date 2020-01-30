455327ba17bb87ba06cac98e4e2a933e5392bc56
Gabriel Ribeiro <gabrielrdavid@gmail.com>
2020-01-30 18:38:58 -0300

    schedule

--- transfer_dash_consulting.py
+++ transfer_dash_consulting.py
@@ -6,6 +6,7 @@ class select_insert:
     def __init__(self):
         self.cursor_sql,   self.con_sql = transfer_dash_connect.ConnectionsDB().Sql_Connection()
         self.cursor_mysql, self.con_mysql = transfer_dash_connect.ConnectionsDB().Mysql_Connection()
+        self.insert_mysql_query_1()
 
     def select_sql_query_1(self):
         try:
@@ -17,9 +18,21 @@ class select_insert:
             return False
 
     def insert_mysql_query_1(self):
-        self.cursor_mysql.executemany(transfer_dash_connect.ConnectionsDB().conf_get()['databases_configs']['mysql']
+        try:
+            self.cursor_mysql.executemany(transfer_dash_connect.ConnectionsDB().conf_get()['databases_configs']['mysql']
                            ['queries']['query_1'], (select_insert().select_sql_query_1()))
-        return self.con_mysql.commit()
+            print("insert")
+            return self.con_mysql.commit()
+        except:
+            print("Problem to query Mysql_1")
+            return False
+
 
 if __name__ == '__main__':
-    select_insert().insert_mysql_query_1()
+    x = select_insert()
+    y = x.insert_mysql_query_1()
+    schedule.every(10).seconds.do(y)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
